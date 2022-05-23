1e5af3aa712beeba78bfd54f19dcce50463bdf90
Your Name <you@example.com>
2022-05-23 17:54:32 +0200

    Done

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBoOBo8RacS_YuLOG3auysSTdEfeCtD7Sc',
+    authDomain: 'linkedin-bf16c.firebaseapp.com',
+    projectId: 'linkedin-bf16c',
+    storageBucket: 'linkedin-bf16c.appspot.com',
+    messagingSenderId: '339494660908',
+    appId: '1:339494660908:web:bc70d9f27cc769d9bf5a89',
+  },
+  baseUrl: 'http://localhost:4200',
+};
